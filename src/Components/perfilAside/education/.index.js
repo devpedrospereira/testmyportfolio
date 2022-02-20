5edefddce6145c086cd7aee640f6c3ea9be9490f
devpedrospereira <pedros.pereira558@gmail.com>
2022-02-20 19:47:26 -0300

    implementação do component experience

--- src/Components/perfilAside/education/.index.js
+++ src/Components/perfilAside/education/.index.js
@@ -5,13 +5,18 @@ export default function Experience(){
 
     return(
         <div className={styles.container}>
-            <h1>Experiências</h1>
+            <h1>Educação</h1>
             <div className={styles.listContacts}>
                 <ul>
                     <li>
-                        <h2>RocketSeat</h2>
-                        <p>2019 - Atualmente</p>
-                        <p>Community Experience</p>
+                        <h2>Secretaria de Educação</h2>
+                        <p>2017 - 2018</p>
+                        <p>Analise e desenvolvimento de sistemas</p>
+                    </li>
+                    <li>
+                        <h2>Instituto Federal</h2>
+                        <p>2018 - 2020</p>
+                        <p>Sistemas para Internet</p>
                     </li>
                     <li>
                         <h2>RocketSeat</h2>
