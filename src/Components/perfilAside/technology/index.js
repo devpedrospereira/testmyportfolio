8793007063b9939be642fb79f2d8e4d67ef7d623
devpedrospereira <pedros.pereira558@gmail.com>
2022-02-21 18:22:15 -0300

    Requisição e renderização dinamica das tecnologias

--- src/Components/perfilAside/technology/index.js
+++ src/Components/perfilAside/technology/index.js
@@ -1,31 +1,26 @@
 import styles from './styles.module.scss'
+import data from '../../../../data/data.json'
+
 
 export default function Technology() {
+    const newdata = JSON.parse(JSON.stringify(data))
 
+    const technologys = newdata.technologys.map((item,index) =>{
+        const {technology} = item
+
+        return(
+            <li key={index}>
+                <div><p>{technology}</p></div>
+            </li>
+        )
+    })
 
     return (
         <div className={styles.container}>
             <h1>Tecnologias</h1>
             <div className={styles.listContacts}>
                 <ul>
-                    <li>
-                        <div><p>JAVASCRIP</p></div>
-                    </li>
-                    <li>
-                        <div><p>REACTJS</p></div>
-                    </li>
-                    <li>
-                        <div><p>NODEJS</p></div>
-                    </li>
-                    <li>
-                        <div><p>GIT</p></div>
-                    </li>
-                    <li>
-                        <div><p>HTML</p></div>
-                    </li>
-                    <li>
-                        <div><p>CSS</p></div>
-                    </li>
+                    {technologys}
                 </ul>
             </div>
         </div>
