cb8a55ae0f1a2622051c6289e4775a3cbca97f00
devpedrospereira <pedros.pereira558@gmail.com>
2022-02-25 16:20:20 -0300

    Finalização do component myProjects para descktop

--- src/Components/contentMain/myProjects/index.js
+++ src/Components/contentMain/myProjects/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from './styles.module.scss'
 import data from '../../../../data/data.json'
 
@@ -6,32 +7,38 @@ export default function MyProjects(){
 
     const cards = newdata.git.map((item,index) =>{
 
-        const {repository, description, stars, works, language} = item;
-        return(
+        const {repository, description, stars, forks, language} = item;
+
+        // o if abaixo, verifica se existe valores dentro das constantes acima, se existir, continuar o return normamente, se não, não retorna nada!
+        if (repository) return(
             <div key={index} className={styles.card}>
                 <div id={styles.repository}>
-                    <img src="./assets/folder.svg" alt="" />
+                    <img src="./assets/folder.svg" alt="Repositório" />
                     <h2>{repository}</h2>
                 </div>
+
                 <div id={styles.description}>
                     <p>{description}</p>
                 </div>
+
                 <div id={styles.statistics}>
                     <div id={styles.nambers}>
-                        <img src="./assets/star.svg" alt="" />
+                        <img src="./assets/star.svg" alt="Estrelas" />
                         <p>{stars}</p>
-                        <img src="./assets/git-branch.svg" alt="" />
-                        <p>{works}</p>
+                        <img src="./assets/git-branch.svg" alt="forcks" />
+                        <p>{forks}</p>
                     </div>
+                    
                     <div id={styles.language}>
+                        
+                        <img src="./assets/ellipse.svg" alt="linguagem" />
                         <p>{language}</p>
                     </div>
                 </div>
             </div>
         )
-
     })
-    // console.log(newdata.git)
+
     return(
         <div className={styles.container}>
             <section>
@@ -39,6 +46,7 @@ export default function MyProjects(){
                     <h1>Meus Projetos</h1>
                     <h2>Veja todos</h2>
                 </header>
+
                 <div className={styles.cards}>
                     {cards}
                 </div>
